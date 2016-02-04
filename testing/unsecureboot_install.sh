cbbf63927cf40b1b92284b37cc80a725472ff77c
Jacob McLaughlin <Jacob.L.H.McLaughlin@gmail.com>
2016-02-04 17:35:13 +1100

    add terminal output to unsecureboot_install

--- testing/unsecureboot_install.sh
+++ testing/unsecureboot_install.sh
@@ -5,3 +5,4 @@ rm /system/etc/init.d/001unsecureboot
 cp /storage/emulated/0/git/2nd-init/system/etc/init.d/001unsecureboot /system/etc/init.d/
 chmod 777 /system/etc/init.d/001unsecureboot
 mount -o ro,remount -t ext4 /system
+ls -l /system/etc/init.d/
