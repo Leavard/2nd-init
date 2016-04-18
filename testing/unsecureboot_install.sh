d4b7db808f956d428fdb653e52a8ab95553b4e52
Jacob McLaughlin <Jacob.L.H.McLaughlin@gmail.com>
2016-04-19 03:16:06 +1000

    delete old install script

--- testing/unsecureboot_install.sh
+++
@@ -1,8 +0,0 @@
-#!/system/bin/sh
-
-mount -o rw,remount -t ext4 /system
-rm /system/etc/init.d/001unsecureboot
-cp /storage/emulated/0/git/2nd-init/system/etc/init.d/001unsecureboot /system/etc/init.d/
-chmod 777 /system/etc/init.d/001unsecureboot
-mount -o ro,remount -t ext4 /system
-ls -l /system/etc/init.d/
